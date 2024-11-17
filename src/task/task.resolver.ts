e548f39e17fd548583594bb4889663c1f7ca47dd
mamitus <85916526+mami-tus@users.noreply.github.com>
2024-11-17 12:09:17 +0900

    セクション3-18. タスク作成機能の実装

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Task } from 'src/task/models/task.model';
 import { TaskService } from 'src/task/task.service';
 
@@ -10,5 +10,16 @@ export class TaskResolver {
   @Query(() => [Task], { nullable: 'items' })
   getTasks(): Task[] {
     return this.taskService.getTasks();
+  }
+
+  @Mutation(() => Task)
+  createTask(
+    @Args('name') name: string, // GraphQLのリクエストから情報を取り出すために@Argsつける
+
+    @Args('dueDate') dueDate: string,
+
+    @Args('description', { nullable: true }) description?: string,
+  ): Task {
+    return this.taskService.createTask(name, dueDate, description);
   }
 }
