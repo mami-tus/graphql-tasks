c111427f0756ba75eae2286d88755d406ff8a11e
mamitus <85916526+mami-tus@users.noreply.github.com>
2024-12-15 15:00:35 +0900

    26. タスクの作成と一覧取得機能の修正 Part2

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Task } from '@prisma/client';
 import { CreateTaskInput } from 'src/task/dto/createTask.input';
-import { Task } from 'src/task/models/task.model';
+import { Task as TaskModel } from 'src/task/models/task.model';
 import { TaskService } from 'src/task/task.service';
 
 @Resolver()
@@ -8,15 +9,15 @@ export class TaskResolver {
   constructor(private taskService: TaskService) {}
 
   // getTasksがGraphQLのデータを取得するためのメソッドであることを伝えるために@Query()デコレータを使用
-  @Query(() => [Task], { nullable: 'items' })
-  getTasks(): Task[] {
-    return this.taskService.getTasks();
+  @Query(() => [TaskModel], { nullable: 'items' })
+  async getTasks(): Promise<Task[]> {
+    return await this.taskService.getTasks();
   }
 
-  @Mutation(() => Task)
-  createTask(
+  @Mutation(() => TaskModel)
+  async createTask(
     @Args('createTaskInput') createTaskInput: CreateTaskInput, // GraphQLのリクエストから情報を取り出すために@Argsつける
-  ): Task {
-    return this.taskService.createTask(createTaskInput);
+  ): Promise<Task> {
+    return await this.taskService.createTask(createTaskInput);
   }
 }
