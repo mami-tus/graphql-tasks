78222e06260a484f575fcfca7661517318c48e9f
mamitus <85916526+mami-tus@users.noreply.github.com>
2025-01-02 10:42:52 +0900

    32. ユーザー作成機能の実装 Part2

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,17 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { User } from '@prisma/client';
+import { CreateUserInput } from 'src/user/dto/createUser.input';
+import { UserService } from 'src/user/user.service';
+import { User as UserModel } from 'src/user/models/user.model';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
+}
